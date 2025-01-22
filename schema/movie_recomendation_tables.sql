d95c125994c80fcd3b9db0faaf74495d5eb86a2b
camilajaviera <cjmunozn@hotmail.com>
2025-01-22 17:31:39 -0300

    feat(tables): edit the file

--- schema/movie_recomendation_tables.sql
+++ schema/movie_recomendation_tables.sql
@@ -26,24 +26,3 @@ CREATE TABLE movie_recommendation.ratings (
     rating DECIMAL(3, 2) CHECK (rating >= 1.0 AND rating <= 5.0),
     rating_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Create Table: watchlist
-CREATE TABLE movie_recommendation.watchlist (
-    watchlist_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES movie_recommendation.users(user_id),
-    movie_id INT REFERENCES movie_recommendation.movies(movie_id),
-    added_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Create Table: genres
-CREATE TABLE movie_recommendation.genres (
-    genre_id SERIAL PRIMARY KEY,
-    genre_name VARCHAR(100) NOT NULL UNIQUE
-);
-
--- Create Table: movie_genres
-CREATE TABLE movie_recommendation.movie_genres (
-    movie_id INT REFERENCES movie_recommendation.movies(movie_id),
-    genre_id INT REFERENCES movie_recommendation.genres(genre_id),
-    PRIMARY KEY (movie_id, genre_id)
-);
