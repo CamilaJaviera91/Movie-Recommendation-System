254747f4fba21ffc94aab7abac4a194091e8c93b
camilajaviera <cjmunozn@hotmail.com>
2025-02-03 15:38:10 -0300

    fix(genre): edit code

--- sources/queries/query_genre_rating.py
+++ sources/queries/query_genre_rating.py
@@ -56,8 +56,6 @@ def query_genre_rating():
         columns = [desc[0] for desc in cursor.description]  # Extract column names from query result
         df = pd.DataFrame(records, columns=columns)  # Create DataFrame
 
-        print(df)  # Print DataFrame to console
-
         return df  # Return DataFrame
 
     except psycopg2.Error as e:
